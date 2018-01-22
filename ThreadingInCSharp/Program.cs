f9d766549b6b46b13f97865b14a5debcd270b3fe
Zhijie Li <ZhijieLi@outlook.com>
2018-01-22 14:21:51 +0800

    BackgroundWorkerSample

---
+++ ThreadingInCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadingInCSharp
+{
+    using System.ComponentModel;
+
+    class Program
+    {
+        static BackgroundWorker _bw = new BackgroundWorker();
+
+        static void Main()
+        {
+            _bw.DoWork += bw_DoWork;
+            _bw.RunWorkerAsync("Message to worker");
+            Console.ReadLine();
+        }
+
+        static void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // This is called on the worker thread
+            Console.WriteLine(e.Argument);        // writes "Message to worker"
+                                                  // Perform time-consuming task...
+        }
+    }
+}
---
+++ ThreadingInCSharpTest/BackgroundWorkerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThreadingInCSharpTest
+{
+    using System.ComponentModel;
+
+    [TestClass]
+    public class BackgroundWorkerTest
+    {
+        static BackgroundWorker _bw = new BackgroundWorker();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _bw.DoWork += bw_DoWork;
+            _bw.RunWorkerAsync("Message to worker");
+        }
+
+        static void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // This is called on the worker thread
+            Console.WriteLine(e.Argument);        // writes "Message to worker"
+                                                  // Perform time-consuming task...
+        }
+    }
+}
